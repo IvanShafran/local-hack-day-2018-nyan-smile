22804deca9842bb472cdb10cdd6da3b35b98f852
Ivan Shafran <shafran.fun@gmail.com>
2018-12-01 11:42:58 +0300

    Настроить fps и размеры

--- app/src/main/java/mlh/hack/CameraSource.java
+++ app/src/main/java/mlh/hack/CameraSource.java
@@ -82,9 +82,9 @@ public class CameraSource {
 
     // These values may be requested by the caller.  Due to hardware limitations, we may need to
     // select close, but not exactly the same values for these.
-    private final float requestedFps = 20.0f;
-    private final int requestedPreviewWidth = 480;
-    private final int requestedPreviewHeight = 360;
+    private final float requestedFps = 60.0f;
+    private final int requestedPreviewWidth = 300;
+    private final int requestedPreviewHeight = 300;
     private final boolean requestedAutoFocus = true;
 
     // These instances need to be held onto to avoid GC of their underlying resources.  Even though
